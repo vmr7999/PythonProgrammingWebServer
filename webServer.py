c8c15395f68f042e13ee04bba79efe3db4cbc08f
vmr7999 <vmr7999@nyu.edu>
2025-06-13 22:13:52 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -39,7 +39,7 @@ def webServer(port=13331):
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
       #Fill in start
-      f = open(filename[1:], "r")
+      f = open(filename[1:], "rb")
       #Fill in end
       
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
